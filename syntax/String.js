835761ff33096997f55919f06f1540fcdb9615f9
KoSangWon <jackoss@naver.com>
2020-02-14 16:09:00 +0900

    nodejs tutorial

---
+++ syntax/String.js
@@ -0,0 +1,5 @@
+console.log('1'+'1');
+
+console.log(
+'Lorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolorLorem ipsum dolor Lorem ipsum dolor'.length
+);
