f32de2ef056f8a41342d3f1b26700f5eb452853b
KoSangWon <jackoss@naver.com>
2020-02-18 13:48:30 +0900

    object

---
+++ syntax/object3.js
@@ -0,0 +1,14 @@
+//서로 연관된 값들을 객체에 넣는다.
+var o = {
+    v1:'v1',
+    v2:'v2',
+    f1:function(){
+        console.log(this.v1);
+    },
+    f2:function(){
+        console.log(this.v2);
+    }
+}
+
+o.f1();
+o.f2();
